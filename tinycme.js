c8cfd6e98597eb6ef0972c8644662910e8bee6ad
malaika <101466672+malakhims@users.noreply.github.com>
2025-09-23 13:28:51 -0400

    Update tinycme.js

--- tinycme.js
+++ tinycme.js
@@ -1,6 +1,7 @@
-/*must make account (i hate it too i know)*/
-/*paste your tinycme in YOURAPIKEY*/
-/*https://www.tiny.cloud/my-account/integrate/#html*/
+// must make account (i hate it too i know)
+// paste your tinycme in YOURAPIKEY
+// https://www.tiny.cloud/my-account/integrate/#html
 
 
 <script src="https://cdn.tiny.cloud/1/YOURAPIKEY/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
