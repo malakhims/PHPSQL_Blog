e259348d1e8d7919c10b688b3d73fd74138de26a
malaika <101466672+malakhims@users.noreply.github.com>
2025-10-05 12:34:48 -0400

    Delete altertable.sql

--- altertable.sql
+++
@@ -1,8 +0,0 @@
--- Add `visible` column if it doesn't exist (for old users)
-ALTER TABLE `logs`
-  ADD COLUMN IF NOT EXISTS `visible` ENUM('y','n') NOT NULL DEFAULT 'y';
-
--- Optional: ensure old rows are updated to a default visible value
-UPDATE `logs`
-SET `visible` = 'y'
-WHERE `visible` IS NULL;
