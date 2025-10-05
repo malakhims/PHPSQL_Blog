b6ca68bbc2d1693ba1c1bd67845e5f05a0abdc73
malaika <101466672+malakhims@users.noreply.github.com>
2025-10-05 12:34:23 -0400

    Update logs.sql

--- logs.sql
+++ logs.sql
@@ -1,68 +1,22 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 23, 2025 at 02:13 PM
--- Server version: 11.4.7-MariaDB-deb12
--- PHP Version: 8.1.32
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `blog`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `logs`
---
-
-CREATE TABLE `logs` (
-  `id` int(11) NOT NULL,
+-- Full table structure with `visible` column for new users
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `post_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `category` varchar(50) DEFAULT 'Uncategorized',
   `tags` varchar(255) DEFAULT '',
-  `anchor_name` varchar(16) NOT NULL DEFAULT 'unknown'
+  `anchor_name` varchar(16) NOT NULL DEFAULT 'unknown',
+  `visible` ENUM('y','n') NOT NULL DEFAULT 'y',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `logs`
---
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `logs`
---
+-- For old users: if the table already exists, just add `visible` column
 ALTER TABLE `logs`
-  ADD PRIMARY KEY (`id`);
+  ADD COLUMN IF NOT EXISTS `visible` ENUM('y','n') NOT NULL DEFAULT 'y';
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `logs`
---
-ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=243;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Set existing NULL values to 'n' so old drafts are hidden
+UPDATE `logs`
+SET `visible` = 'n'
+WHERE `visible` IS NULL;
