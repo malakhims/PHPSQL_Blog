4561b96756b3d72fc3c32254b430f5006c71f226
malaika <101466672+malakhims@users.noreply.github.com>
2026-06-24 02:04:50 -0400

    FUCKING SQL FILE

--- logs.sql
+++ logs.sql
@@ -1,22 +1,65 @@
--- Full table structure with `visible` column for new users
-CREATE TABLE IF NOT EXISTS `logs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 24, 2026 at 07:51 AM
+-- Server version: 11.4.7-MariaDB-deb12
+-- PHP Version: 8.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
   `title` varchar(255) DEFAULT NULL,
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` text DEFAULT NULL,
   `post_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
   `category` varchar(50) DEFAULT 'Uncategorized',
   `tags` varchar(255) DEFAULT '',
   `anchor_name` varchar(16) NOT NULL DEFAULT 'unknown',
-  `visible` ENUM('y','n') NOT NULL DEFAULT 'y',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `visible` enum('y','n') NOT NULL DEFAULT 'y'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- For old users: if the table already exists, just add `visible` column
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logs`
+--
 ALTER TABLE `logs`
-  ADD COLUMN IF NOT EXISTS `visible` ENUM('y','n') NOT NULL DEFAULT 'y';
+  ADD PRIMARY KEY (`id`);
 
--- Set existing NULL values to 'n' so old drafts are hidden
-UPDATE `logs`
-SET `visible` = 'n'
-WHERE `visible` IS NULL;
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
